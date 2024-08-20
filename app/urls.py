18697105b73fda344b01c4cdd5f87737e9697edc
asilbek-ismoilov <asilbe.m00d@gmail.com>
2024-08-20 22:50:17 +0300

    Django project 4

---
+++ app/urls.py
@@ -0,0 +1,45 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import index_view,about_view, appointment_view, blog_details_view, blog_grid_view, blog_view, cart_grocery_view, cart_pharmacy_view, cart_view, checkout_grocery_view, checkout_pharmacy_view, checkout_view, coming_soon_view, contact_view, dashboard_view, error_view, faq_view, grocery_details_view, grocery_grocery_product_view, grocery_product_view, grocery_view, home_startup_view, pharmacy_details_view, pharmacy_pharmacy_product_view, pharmacy_product_view, pharmacy_view, portfolio_2_view, portfolio_details_view, portfolio_view, product_details_view, service_view, services_view, store_view, wishlist_grocery_view, wishlist_pharmacy_view, wishlist_view
+
+urlpatterns = [
+    path('',index_view,name='home-page'),
+    path('about/',about_view,name='about-page'),
+    path('appointment/',appointment_view,name='appointment-page'),
+    path('blog-details/',blog_details_view,name='blog-details-page'),
+    path('blog-grid/',blog_grid_view,name='blog-grid-page'),
+    path('blog/',blog_view,name='blog-page'),
+    path('cart-grocery/',cart_grocery_view,name='cart-grocery-page'),
+    path('cart-pharmacy/',cart_pharmacy_view,name='cart-pharmacy-page'),
+    path('cart/',cart_view,name='cart-page'),
+    path('checkout-grocery/',checkout_grocery_view,name='checkout-grocery-page'),
+    path('checkout-pharmacy/',checkout_pharmacy_view,name='checkout-pharmacy-page'),
+    path('checkout/',checkout_view,name='checkout-page'),
+    path('coming_soon/',coming_soon_view,name='coming_soon-page'),
+    path('contact/',contact_view,name='contact-page'),
+    path('dashboard/',dashboard_view,name='dashboard-page'),
+    path('error/',error_view,name='error-page'),
+    path('faq/',faq_view,name='faq-page'),
+    path('grocery_details/',grocery_details_view,name='grocery_details-page'),
+    path('grocery-grocery-product/',grocery_grocery_product_view,name='grocery-grocery-product-page'),
+    path('grocery-product/',grocery_product_view,name='grocery-product-page'),
+    path('grocery/',grocery_view,name='grocery-page'),
+    path('home_startup/',home_startup_view,name='home_startup-page'),
+    path('pharmacy-details/',pharmacy_details_view,name='pharmacy-details-page'),
+    path('pharmacy-pharmacy/',pharmacy_pharmacy_product_view,name='pharmacy-pharmacy-page'),
+    path('pharmacy_product/',pharmacy_product_view,name='pharmacy-product-page'),
+    path('pharmacy/',pharmacy_view,name='pharmacy-page'),
+    path('portfolio-2/',portfolio_2_view,name='portfolio-2-page'),
+    path('portfolio-details/',portfolio_details_view,name='portfolio-details-page'),
+    path('portfolio/',portfolio_view,name='portfolio-page'),
+    path('product-details/',product_details_view,name='product-details-page'),
+    path('service/',service_view,name='service-page'),
+    path('services/',services_view,name='services-page'),
+    path('store/',store_view,name='store-page'),
+    path('wishlist-grocery/',wishlist_grocery_view,name='wishlist-grocery-page'),
+    path('wishlist-pharmacy/',wishlist_pharmacy_view,name='wishlist-pharmacy-page'),
+    path('wishlist.html/',wishlist_view,name='wishlist.html-page'),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ app/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render
+
+def about_view(request):
+    return render(request, "about.html")
+
+def appointment_view(request):
+    return render(request, "appointment.html")
+
+def blog_details_view(request):
+    return render(request, "blog-details.html")
+
+def blog_grid_view(request):
+    return render(request, "blog-grid.html")
+
+def blog_view(request):
+    return render(request, "blog.html")
+
+def cart_grocery_view(request):
+    return render(request, "cart-grocery.html")
+
+def cart_pharmacy_view(request):
+    return render(request, "cart-pharmacy.html")
+
+def cart_view(request):
+    return render(request, "cart.html")
+
+def checkout_grocery_view(request):
+    return render(request, "checkout-grocery.html")
+
+def checkout_pharmacy_view(request):
+    return render(request, "checkout-pharmacy.html")
+
+def checkout_view(request):
+    return render(request, "checkout.html")
+
+def coming_soon_view(request):
+    return render(request, "coming_soon.html")
+
+def contact_view(request):
+    return render(request, "contact.html")
+
+def dashboard_view(request):
+    return render(request, "dashboard.html")
+
+def error_view(request):
+    return render(request, "error.html")
+
+def faq_view(request):
+    return render(request, "faq.html")
+
+def grocery_details_view(request):
+    return render(request, "grocery-details.html")
+
+def grocery_grocery_product_view(request):
+    return render(request, "grocery-grocery-product.html")
+
+def grocery_product_view(request):
+    return render(request, "grocery-product.html")
+
+def grocery_view(request):
+    return render(request, "grocery.html")
+
+def home_startup_view(request):
+    return render(request, "home-startup.html")
+#
+def index_view(request):
+    return render(request, "index.html")
+#
+def pharmacy_details_view(request):
+    return render(request, "pharmacy-details.html")
+
+def pharmacy_pharmacy_product_view(request):
+    return render(request, "pharmacy-pharmacy-product.html")
+
+def pharmacy_product_view(request):
+    return render(request, "pharmacy-product.html")
+
+def pharmacy_view(request):
+    return render(request, "pharmacy.html")
+
+def portfolio_2_view(request):
+    return render(request, "portfolio-2.html")
+
+def portfolio_details_view(request):
+    return render(request, "portfolio-details.html")
+
+def portfolio_view(request):
+    return render(request, "portfolio.html")
+
+def product_details_view(request):
+    return render(request, "product-details.html")
+
+def service_view(request):
+    return render(request, "service-page")
+
+def services_view(request):
+    return render(request, "services.html")
+
+def store_view(request):
+    return render(request, "store.html")
+
+def wishlist_grocery_view(request):
+    return render(request, "wishlist-grocery.html")
+
+def wishlist_pharmacy_view(request):
+    return render(request, "wishlist-pharmacy.html")
+
+def wishlist_view(request):
+    return render(request, "wishlist.html")
+
